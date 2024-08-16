1cdd6dbf3babeab78fc1414a12585e65244f1263
admin <kirill.33wh@gmail.com>
2024-08-16 18:43:27 +0100

    circle moves on x and y

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,10 +3,16 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
     int circleYCords;
+    int circleXCords;
+    boolean isReachedRightEnd;
+    boolean isReachedLeftEnd;
     boolean isGameRunning;
     boolean isReachedBottom;
     boolean isReachedTop = true;
     GamePanel(){
+        isReachedLeftEnd = true;
+        circleXCords = 20;
+        circleYCords = 75;
         isGameRunning = true;
         System.out.println("gsame started");
     }
@@ -14,22 +20,25 @@ public class GamePanel extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.setColor(Color.magenta);
-        g.drawRect(20, circleYCords,50,50);
         setBackground(Color.black);
         System.out.println(circleYCords);
         g.setColor(Color.white);
         g.fillRect(20, 50, 750, 15);
         g.fillRect(20, 500, 750, 15);
 
-        g.drawLine(0,500, 900, 500);
         g.setColor(Color.red);
-        g.fillOval(20, circleYCords, 50, 50);
+        g.fillOval(circleXCords, circleYCords, 50, 50);
         if(isReachedBottom){
-            circleYCords = circleYCords - 5; // Going UP
-            if(circleYCords <= 50) {
+            circleYCords = circleYCords - 5;// Going UP
+            if(circleYCords <= 75) {
                 isReachedBottom = false;
                 isReachedTop = true;
+            }
+            if(isReachedRightEnd){
+                circleXCords = circleXCords - 2;
+            }
+            if(isReachedLeftEnd){
+                circleXCords = circleXCords + 2;
             }
         }
         if(isReachedTop) {
@@ -38,6 +47,27 @@ public class GamePanel extends JPanel {
                 isReachedBottom = true;
                 isReachedTop = false;
             }
+            if(isReachedRightEnd){
+                circleXCords = circleXCords - 2;
+            }
+            if(isReachedLeftEnd){
+                circleXCords = circleXCords + 2;
+            }
+        }
+        if(circleXCords >= 750){
+            isReachedRightEnd = true;
+            isReachedLeftEnd = false;
+        }
+        if(isReachedRightEnd){
+           circleXCords = circleXCords - 2;
+
+        }
+        if(circleXCords <= 20){
+            isReachedLeftEnd = true;
+            isReachedRightEnd = false;
+        }
+        if(isReachedLeftEnd){
+            circleXCords = circleXCords + 2;
         }
     }
     public void startGame(){
