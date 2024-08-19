4829768ffef026c0f7b688ce5a42ec6326fb7af3
admin <kirill.33wh@gmail.com>
2024-08-19 19:04:12 +0100

    circle moves on x and y

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class GamePanel extends JPanel {
     Random random = new Random();
+    String circleCollision;
     int deltaDirection;
     int speed;
     int circleYCords;
@@ -19,8 +20,9 @@ public class GamePanel extends JPanel {
         circleYCords = 75;
         speed = 5;
         deltaDirection = 5;
+        circleCollision = "NONE";
         isGameRunning = true;
-        System.out.println("gsame started");
+        //System.out.println("gsame started");
     }
 
     @Override
@@ -33,60 +35,103 @@ public class GamePanel extends JPanel {
         g.fillRect(20, 500, 750, 15);
         g.fillRect(755, 65, 15, 450);
         g.fillRect(20, 65, 15, 450);
-
-
         g.setColor(Color.red);
         g.fillOval(circleXCords, circleYCords, 50, 50);
-        if(isReachedBottom){
-            circleYCords = circleYCords - speed;// Going UP
-            if(circleYCords <= 65) {
-                isReachedBottom = false;
-                isReachedTop = true;
-                speed = random.nextInt(5,20);
+
+        checkBallCollision();
+        switch (circleCollision) {
+            case "NONE" -> {
+
             }
-            if(isReachedRightEnd){
-                circleXCords = circleXCords - deltaDirection;
-            }
-            if(isReachedLeftEnd){
+            case "TOP" -> {
+                circleYCords = circleYCords + speed;
                 circleXCords = circleXCords + deltaDirection;
             }
-        }
-        if(isReachedTop) {
-            circleYCords = circleYCords + speed;   // GOING Down
-            if(circleYCords >= 450){
-                isReachedBottom = true;
-                isReachedTop = false;
-                speed = random.nextInt(5,20);
-            }
-            if(isReachedRightEnd){
+            case "BOTTOM" -> {
+                circleYCords = circleYCords - speed;
                 circleXCords = circleXCords - deltaDirection;
             }
-            if(isReachedLeftEnd){
+            case "LEFT" -> {
                 circleXCords = circleXCords + deltaDirection;
+                circleYCords = circleYCords + speed;
             }
-        }
-        if(circleXCords >= 700){
-            isReachedRightEnd = true;
-            isReachedLeftEnd = false;
-            speed = random.nextInt(5,20);
-            deltaDirection = random.nextInt(5,10);
-        }
-        if(isReachedRightEnd){
-           circleXCords = circleXCords - deltaDirection;
-        }
-        if(circleXCords <= 30){
-            isReachedLeftEnd = true;
-            isReachedRightEnd = false;
-            speed = random.nextInt(5,20);
-            deltaDirection = random.nextInt(5,10);
-        }
-        if(isReachedLeftEnd){
-            circleXCords = circleXCords + deltaDirection;
+            case "RIGHT" -> {
+                circleXCords = circleXCords - deltaDirection;
+                circleYCords = circleYCords - speed;
+            }
         }
 
-        System.out.println("Speed:" + speed);
-        System.out.println(isReachedLeftEnd + " " + isReachedRightEnd);
+//        if(isReachedBottom){
+//            circleYCords = circleYCords - speed;// Going UP
+//            if(circleYCords <= 65) {
+//                isReachedBottom = false;
+//                isReachedTop = true;
+//                speed = random.nextInt(5,20);
+//            }
+//            if(isReachedRightEnd){
+//                circleXCords = circleXCords - deltaDirection;
+//            }
+//            if(isReachedLeftEnd){
+//                circleXCords = circleXCords + deltaDirection;
+//            }
+//        }
+//        if(isReachedTop) {
+//            circleYCords = circleYCords + speed;   // GOING Down
+//            if(circleYCords >= 450){
+//                isReachedBottom = true;
+//                isReachedTop = false;
+//                speed = random.nextInt(5,20);
+//            }
+//            if(isReachedRightEnd){
+//                circleXCords = circleXCords - deltaDirection;
+//            }
+//            if(isReachedLeftEnd){
+//                circleXCords = circleXCords + deltaDirection;
+//            }
+//        }
+//        if(circleXCords >= 700){
+//            isReachedRightEnd = true;
+//            isReachedLeftEnd = false;
+//            speed = random.nextInt(5,20);
+//            deltaDirection = random.nextInt(5,10);
+//        }
+//        if(isReachedRightEnd){
+//           circleXCords = circleXCords - deltaDirection;
+//        }
+//        if(circleXCords <= 30){
+//            isReachedLeftEnd = true;
+//            isReachedRightEnd = false;
+//            speed = random.nextInt(5,20);
+//            deltaDirection = random.nextInt(5,10);
+//        }
+//        if(isReachedLeftEnd){
+//            circleXCords = circleXCords + deltaDirection;
+//        }
+
+        //System.out.println("Speed:" + speed);
+        //System.out.println(isReachedLeftEnd + " " + isReachedRightEnd);
     }
+
+    public void checkBallCollision(){
+        if(circleYCords <= 65){
+            circleCollision = "TOP";
+            speed = random.nextInt(5,20);
+            deltaDirection = random.nextInt(5,10);
+        } else if (circleYCords >= 450) {
+            circleCollision = "BOTTOM";
+            speed = random.nextInt(5,20);
+            deltaDirection = random.nextInt(5,10);
+        } else if (circleXCords <= 30) {
+            circleCollision = "LEFT";
+            speed = random.nextInt(5,20);
+            deltaDirection = random.nextInt(5,10);
+        } else if (circleXCords >= 700) {
+            circleCollision = "RIGHT";
+            speed = random.nextInt(5,20);
+            deltaDirection = random.nextInt(5,10);
+        }
+    }
+
     public void startGame(){
         while(isGameRunning){
             repaint();
