d8c13f491abeabaa87b9e61293b5c97f0a7d81d3
admin <kirill.33wh@gmail.com>
2024-10-07 18:59:45 +0100

    all essentials are done

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,12 +3,17 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class GamePanel extends JPanel implements MouseMotionListener {
 
     Random random = new Random();
     String circleCollision;
+    final int PLAYER_WIDTH = 30;
+    final int PLAYER_HEIGHT = 30;
+
     int deltaDirection;
     int speed;
     int circleYCords;
@@ -24,22 +29,38 @@ public class GamePanel extends JPanel implements MouseMotionListener {
     Ellipse2D ellipse;
     String[] sides = {"TOP", "BOTTOM", "RIGHT", "LEFT"};
     JButton button;
-    int score;
+    int level;
     Window window;
+    int paintCount = 120;
+    int minutes = 2;
+    int seconds = 0;
+    Thread countThread;
+    int lvlSpeed;
+    boolean isFruitEaten;
+    Graphics2D graphic2d;
+    Random randomObj;
+    double fruitCordsX;
+    double fruitCordsY;
+    Shape starObj;
+    int score = -1;
 
     GamePanel(Window window) {
         this.window = window;
         isReachedLeftEnd = true;
         button = new JButton();
+        randomObj = new Random();
         circleXCords = Window.WIDTH / 2;
+        level = 1;
         circleYCords = 75;
 //        System.out.println(circleXCords);
         ellipse = new Ellipse2D.Float(circleXCords, circleYCords, 50, 50);
         speed = 5;
         deltaDirection = 5;
         circleCollision = "BOTTOM";
-        playerX = 400 - 30;
-        playerY = 300 - 30;
+        playerX = 400 - PLAYER_WIDTH;
+        playerY = 300 - PLAYER_HEIGHT;
+        lvlSpeed = 35;
+        isFruitEaten = true;
         isGameRunning = true;
         addMouseMotionListener(this);
     }
@@ -96,20 +117,28 @@ public class GamePanel extends JPanel implements MouseMotionListener {
     public void paint(Graphics g) {
         super.paint(g);
 
-        gameOver(g);
+        graphic2d = (Graphics2D) g;
+        g.setColor(Color.green);
+        g.setFont(new Font("ARIAL", Font.BOLD, 30));
+        g.drawString("Score: " + score, 100, 30);
+        g.drawString("timer: " + minutes + ":" + seconds, 600, 30);
+        g.drawString("LEVEL " + level, Window.WIDTH / 2 - 50, 30);
         ellipse.setFrame(circleXCords, circleYCords, 50, 50);
-        gameOver = ellipse.intersects(playerX, playerY, 30, 30);
+        gameOver = ellipse.intersects(playerX, playerY, PLAYER_WIDTH, PLAYER_HEIGHT);
 
 
         //g.drawLine(20, 500, circleXCords, circleYCords);
 
         setBackground(Color.black);
         //sides
-        g.setColor(Color.white);
+        g.setColor(Color.red);
         g.fillRect(20, 50, 750, 15); //TOP
         g.fillRect(20, 500, 750, 15);//BOTTOM
         g.fillRect(755, 65, 15, 450);//RIGHT
         g.fillRect(20, 65, 15, 450);//LEFT
+
+        //will generate a fruit in a random area
+        fruitGenerator(g);
 
         //ball movement
         if (!gameOver) {
@@ -119,15 +148,21 @@ public class GamePanel extends JPanel implements MouseMotionListener {
 
             //player
             g.setColor(Color.green);
-            g.fillRect(playerX, playerY, 30, 30);
+            g.fillRect(playerX, playerY, PLAYER_WIDTH, PLAYER_HEIGHT);
             //System.out.println(playerX + ", " + playerY);
-        } else {
+
+        }
+        //Game over
+        else {
             isGameRunning = false;
+            countThread.interrupt();
             window.gameOver();
         }
 
         //System.out.println("Speed:" + speed);
         //System.out.println(isReachedLeftEnd + " " + isReachedRightEnd);
+
+
     }
 
     // this is a method definition
@@ -136,23 +171,111 @@ public class GamePanel extends JPanel implements MouseMotionListener {
         deltaDirection = random.nextInt(5, 10);
     }
 
+
     public void startGame() {
         isGameRunning = true;
         gameOver = false;
+        startCountdown();
 
         while (isGameRunning) {
-
             repaint();
             try {
-                Thread.sleep(20);
+                Thread.sleep(lvlSpeed);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
-    public void gameOver(Graphics g) {
+    private void startCountdown() {
+        countThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!countThread.isInterrupted()) {
 
+//                    System.out.println("minutes "+minutes+" seconds "+seconds);
+                    if (minutes <= 0 && seconds <= 0) {
+                        level = level + 1;
+                        minutes = 2;
+                        seconds = 0;
+                        if(lvlSpeed <= 5) {
+                            // TODO: User wins the gmae: wins permanent trophy and its stored in file (hdd)
+                            // FILE HANDLING IN JAVA
+                        }
+                        else{
+                            lvlSpeed = lvlSpeed - 1;
+                        }
+                    }
+                    if (seconds % 60 == 0) {
+                        minutes = minutes - 1;
+                        seconds = 59;
+                    }
+
+
+                    else {
+                        seconds = seconds - 1;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        });
+        countThread.start();
+    }
+
+    public void fruitGenerator(Graphics g){
+        g.setColor(Color.YELLOW);
+        System.out.println(fruitCordsX + "," + fruitCordsY);
+        if(isFruitEaten){
+            fruitCordsX = randomObj.nextInt(30,750);
+            fruitCordsY = randomObj.nextInt(70, 480);
+            starObj = createStar(fruitCordsX, fruitCordsY, 10, 20, 10, 50);
+            score += 1;
+            isFruitEaten = false;
+        }
+        if(starObj.intersects(playerX, playerY, PLAYER_WIDTH, PLAYER_HEIGHT)){
+            isFruitEaten = true;
+        }
+        graphic2d.fill(starObj);
+    }
+
+    private static Shape createStar(double centerX, double centerY,
+                                    double innerRadius, double outerRadius, int numRays,
+                                    double startAngleRad)
+    {
+        Path2D path = new Path2D.Double();
+        double deltaAngleRad = Math.PI / numRays;
+        for (int i = 0; i < numRays * 2; i++)
+        {
+            double angleRad = startAngleRad + i * deltaAngleRad;
+            double ca = Math.cos(angleRad);
+            double sa = Math.sin(angleRad);
+            double relX = ca;
+            double relY = sa;
+            if ((i & 1) == 0)
+            {
+                relX *= outerRadius;
+                relY *= outerRadius;
+            }
+            else
+            {
+                relX *= innerRadius;
+                relY *= innerRadius;
+            }
+            if (i == 0)
+            {
+                path.moveTo(centerX + relX, centerY + relY);
+            }
+            else
+            {
+                path.lineTo(centerX + relX, centerY + relY);
+            }
+        }
+        path.closePath();
+        return path;
     }
 
 
--- src/Window.java
+++ src/Window.java
@@ -1,63 +1,83 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Window implements ActionListener {
+public class Window extends JFrame {
 
     public static int WIDTH = 800;
     public static int HEIGHT = 600;
-    JFrame jFrame;
-
     GamePanel gamePanel;
     JPanel gameOverPanel;
     JButton restartButton;
     JLabel jLabel;
-    BorderLayout layout;
+    BoxLayout layout;
+    Window windowObj;
+
 
     Window() {
         //the panels
-        jFrame = new JFrame();
+        windowObj = this;
         gameOverPanel = new JPanel();
-        gamePanel = new GamePanel(this);
+        gamePanel = new GamePanel(windowObj);
 
         jLabel = new JLabel();
-        layout = new BorderLayout();
+        layout = new BoxLayout(gameOverPanel, BoxLayout.Y_AXIS);
         restartButton = new JButton();
         restartButton.setText("restart");
-        restartButton.addActionListener(this);
-        gameOverPanel.setBackground(Color.red);
-       // gameOverPanel.setLayout(layout);
-        //jLabel.setForeground(Color.red);
+        restartButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gamePanel = new GamePanel(windowObj); // created a new object gamepanel and named it the same name
+                display(gamePanel);
+                setTimeout(() -> gamePanel.startGame(), 100);
+            }
+        });
+        gameOverPanel.setBackground(Color.black);
+        gameOverPanel.setBorder(new EmptyBorder(new Insets(250, 250, 250, 200)));
+        gameOverPanel.setLayout(layout);
+        jLabel.setBackground(Color.GREEN);
+        jLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         jLabel.setPreferredSize(new Dimension(330, 100));
         jLabel.setFont(new Font("arial", Font.BOLD, 50));
+        jLabel.setForeground(Color.red);
         jLabel.setText("GAME OVER");
+        gameOverPanel.add(jLabel);
         gameOverPanel.add(restartButton);
-        gameOverPanel.add(jLabel,BorderLayout.CENTER);
 
-        jFrame.add(gameOverPanel);
-        jFrame.add(gamePanel);
-        jFrame.setSize(800, 600);
-        jFrame.setBackground(Color.black);
-        jFrame.setVisible(true);
-        jFrame.setResizable(false);
-
+        add(gameOverPanel);
+        add(gamePanel);
+        setSize(800, 600);
+        setBackground(Color.red);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         gamePanel.startGame();
     }
 
     public void gameOver(){
-        System.out.println("GAEM OVER");
-        jFrame.getContentPane().removeAll();
-        jFrame.getContentPane().add(gameOverPanel);
-        jFrame.repaint();
-        jFrame.printAll(jFrame.getGraphics());
+        display(gameOverPanel);
     }
 
     public static void main(String[] args) {
         new Window();
+    }
+
+
+
+    public void display(JPanel a){
+
+        getContentPane().removeAll();
+        getContentPane().add(a);
+        repaint();
+        printAll(getGraphics());
+
     }
 
     public static void setTimeout(Runnable runnable, int delay){
@@ -70,15 +90,5 @@ public class Window implements ActionListener {
                 System.err.println(e);
             }
         }).start();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        gamePanel = new GamePanel(this); // created a new object gamepanel and named it the same name
-        jFrame.getContentPane().removeAll();
-        jFrame.getContentPane().add(gamePanel);
-        jFrame.repaint();
-        jFrame.printAll(jFrame.getGraphics());
-        setTimeout(() -> gamePanel.startGame(), 1000);
     }
 }
